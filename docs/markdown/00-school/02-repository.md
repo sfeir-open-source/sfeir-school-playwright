925875e8d5c8ee6977d12063488feeb6f4790d0f
Anthony Pena <anthony.pena@outlook.fr>
2024-11-28 17:01:54 +0100

    wip create files

---
+++ docs/markdown/00-school/02-repository.md
@@ -0,0 +1,11 @@
+# Repository
+
+https://github.com/sfeir-open-source/sfeir-school-playwright <!-- .element: class="center" -->
+
+##==##
+
+# Requirements
+
+- Node v20
+- npm
+- A IDE: VSCode or WebStorm recommanded
--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -2,16 +2,42 @@ import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-s
 
 // One method per module
 function schoolSlides() {
-  return ['00-school/00-TITLE.md', '00-school/speaker-ape.md'];
+  const dir = '00-school';
+  return [
+    `${dir}/00-TITLE.md`,
+    `${dir}/01-wifi.md`,
+    `${dir}/02-repository.md`,
+    `${dir}/10-speaker-ape.md`,
+    `${dir}/11-tour-de-table.md`,
+    `${dir}/20-what-we-will-do.md`,
+  ];
 }
 
 function introSlides() {
-  return ['intro/00-TITLE.md', 'intro/00-lab-example.md'];
+  const dir = '01-intro';
+  function installation() {
+    return [`${dir}/00-lab-example.md`];
+  }
+  function appDiscovery() {
+    return [];
+  }
+  function jsRemindersFunctions() {
+    return [];
+  }
+  function jsRemindersAsyncAwait() {
+    return [];
+  }
+  return [
+    `${dir}/00-TITLE.md`,
+    ...installation(),
+    ...appDiscovery(),
+    ...jsRemindersFunctions(),
+    ...jsRemindersAsyncAwait(),
+  ];
 }
 
 function formation() {
   return [
-    //
     ...schoolSlides(), //
     ...introSlides(), //
   ].map((slidePath) => {
